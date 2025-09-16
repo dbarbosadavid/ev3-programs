4077f37c6469f912d55c9743d5e1dc9c47fe0e8f
dbarbosadavid <david.canal123@gmail.com>
2025-09-16 18:59:53 -0300

    Q-Learning 2 - Apresentando resultados

--- segue-linha-padrao/q_learning2.py
+++ segue-linha-padrao/q_learning2.py
@@ -6,6 +6,9 @@ from pybricks.parameters import Port, Button, Stop
 from pybricks.tools import wait
 import random
 
+
+
+
 # Criação dos objetos
 ev3 = EV3Brick()
 
@@ -18,18 +21,18 @@ ref_direita = color_direita.reflection()
 motor_esquerda = Motor(Port.A)
 motor_direita = Motor(Port.B)
 
-velocidade = 200
+velocidade = 100
 black = 8
 white = 69
 treshold = 0.1
 
-def virar_direita(error):
-    motor_esquerda.run(velocidade * error)
-    motor_direita.run(-(velocidade * error) / 2)
+def virar_direita():
+    motor_esquerda.run(velocidade // 2)
+    motor_direita.run(velocidade)
 
-def virar_esquerda(error):
-    motor_esquerda.run(-(velocidade * -error) / 2)
-    motor_direita.run(velocidade * -error)
+def virar_esquerda():
+    motor_esquerda.run(velocidade)
+    motor_direita.run(velocidade // 2)
 
 def seguir_em_frente():
     motor_esquerda.run(velocidade)
@@ -38,13 +41,20 @@ def seguir_em_frente():
 
 actions = ["forward", "left", "right"]
 Q = {}
-alpha, gamma, epsilon = 0.5, 0.8, 0.5  # hiperparâmetros
+alpha, gamma, epsilon = 0.5, 0.8, 1  # hiperparâmetros
 
 def get_state():
-    l = 1 if color_esquerda.reflection() < 45 else 0
-    r = 1 if color_direita.reflection() < 45 else 0
-    erro = color_esquerda.reflection() - color_direita.reflection()
-    return erro, l, r
+    l = color_esquerda.reflection()
+    r = color_direita.reflection()
+    
+    if l < 50 and r < 50:   # ambos no preto
+        return "both_black"
+    elif l < 30:            # só esquerda no preto
+        return "left_black"
+    elif r < 30:            # só direita no preto
+        return "right_black"
+    else:                   # ambos no branco
+        return "lost"
 
 def choose_action(state):
     if random.random() < epsilon or state not in Q:
@@ -52,25 +62,53 @@ def choose_action(state):
     return max(Q[state], key=Q[state].get)
 
 def perform_action(action):
+    if state == "right_black":
+        # gira procurando a linha
+        motor_esquerda.run(100)
+        motor_direita.run(10)
+        wait(1000)
+        motor_esquerda.stop()
+        motor_direita.stop()
+        return
+    elif state == "left_black":
+        motor_esquerda.run(10)
+        motor_direita.run(100)
+        wait(1000)
+        motor_esquerda.stop()
+        motor_direita.stop()
+        return
+    elif state == "lost":
+        motor_esquerda.run(100)
+        motor_direita.run(-100)
+        wait(400)
+        motor_esquerda.stop()
+        motor_direita.stop()
     if action == "forward":
         seguir_em_frente()
     elif action == "left":
-        virar_esquerda(1)  # força de curva simplificada
+        virar_esquerda()  # força de curva simplificada
     elif action == "right":
-        virar_direita(1)
-    wait(200)
+        virar_direita()
+    
+    wait(100)
+    motor_esquerda.stop()
+    motor_direita.stop()
 
 def reward(state):
-    erro, l, r = state
-    if 0 <= abs(erro) <= 5 and (l == 1 or r == 1):   # dois sensores no preto
-        return 5
-    elif 6 <= abs(erro) <= 10:
-        return 1
+    if state == "both_black":
+        return 20   # perfeito
+    elif state in ["left_black", "right_black"]:
+        return 5   # está corrigindo
     else:
-        return -5
+        return -20  # saiu da linha
+
+    
 
 # Loop principal
 while True:
+    
+    epsilon = max(0.1, epsilon * 0.996)
+    print(epsilon)
     state = get_state()
     action = choose_action(state)
     perform_action(action)
